7a2a85c7b500c7123011dd5fb124f3404a53616a
Fernando Gomes <lfgsnando@gmail.com>
2020-03-29 22:14:07 -0300

    first commit

---
+++ TrollKit.xcframework/macos-x86_64/TrollKit.framework/Versions/A/Headers/TrollKit.h
@@ -0,0 +1,19 @@
+//
+//  TrollKit.h
+//  TrollKit
+//
+//  Created by Fernando Gomes on 29/03/20.
+//  Copyright © 2020 Fernando Gomes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TrollKit.
+FOUNDATION_EXPORT double TrollKitVersionNumber;
+
+//! Project version string for TrollKit.
+FOUNDATION_EXPORT const unsigned char TrollKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrollKit/PublicHeader.h>
+
+
